e8ac35a8e8cd8f8e71a4507bc22b18d2e3ee16fc
aliciastudies <6722@holbertonstudents.com>
2023-11-29 21:42:22 +1100

    fix: updated __init__() to convert kwargs from string rep to object rep

--- models/base_model.py
+++ models/base_model.py
@@ -14,22 +14,25 @@ class BaseModel:
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            """Instantiates a new model"""
+            if not kwargs:
+                from models import storage
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            else:
+                for key, value in kwargs.items():
+                    if key != '__class__':
+                        setattr(self, k, v)
+                #convert string rep to objects
+                self.created_at = datetime.strptime(self.created_at,
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+                self.updated_at = datetime.strptime(self.updated_at,
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            # if no id, generate new UUID
+            if not getattr(self, 'id', None):
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
