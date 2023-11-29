11b59bb2bda6ebd4f4eec2f01ea698f49ee316da
aliciastudies <6722@holbertonstudents.com>
2023-11-29 22:28:52 +1100

    feat: added public instance method def delete(self)

--- models/base_model.py
+++ models/base_model.py
@@ -58,3 +58,8 @@ class BaseModel:
         if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """Deletes the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
