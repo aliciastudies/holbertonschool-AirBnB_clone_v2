7cf59fc6f214dd163b241051c0bd2f4a81694c92
EsmeraldaMar <6580@holbertonstudents.com>
2023-11-30 00:09:00 +1100

    fix: added import place in city.py

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 import os
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
