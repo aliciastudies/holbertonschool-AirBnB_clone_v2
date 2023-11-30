095416ab15bef8b72d9b1677d99ae3b7a69c0e18
EsmeraldaMar <6580@holbertonstudents.com>
2023-11-30 12:24:13 +1100

    feat: added relationship in place

--- models/place.py
+++ models/place.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, null
+from sqlalchemy import Table, MetaData
+from sqlalchemy.orm import relationship
+import os
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+metadata = MetaData()
+
+
+class Place(BaseModel, Base):
+    """ Place class """
+    __tablename__ = 'places'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=False)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True, default=null())
+        longitude = Column(Float, nullable=True, default=null())
+        amenity_ids = []
