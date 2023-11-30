d2f63f45da2d33953265f340dcde36ac7fdefb75
EsmeraldaMar <6580@holbertonstudents.com>
2023-11-30 12:51:39 +1100

    fix: place description to null allowed

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, null
 from sqlalchemy import Table, MetaData
 from sqlalchemy.orm import relationship
@@ -17,7 +17,7 @@ class Place(BaseModel, Base):
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=False)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
