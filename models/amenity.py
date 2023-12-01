3f07338e8622a821c7cc8f5a5cb2b110e35a60f1
aliciastudies <6722@holbertonstudents.com>
2023-12-01 16:14:07 +1100

    fix: added place_amenity table

--- models/amenity.py
+++ models/amenity.py
@@ -8,7 +8,16 @@ import os
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
+    
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenity = Table(
+            "place_amenity",
+            Base.metadata,
+            Column('place_id', String(60), ForeignKey("places.id"),
+            primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey("amenities.id"),
+            primary_key=True, nullable=False)
+        )
         name = Column(String(128), nullable=False)
         # place_amenities = represent a relationship Many-To-Many with Place
         place_amenities = relationship("Place", secondary="place_amenity",
