0e2dc1cdc4e048fc4f10a675c1b2b84eb1958626
EsmeraldaMar <6580@holbertonstudents.com>
2023-12-01 14:02:21 +1100

    fix: commented out all relationships and pycode in review.py

--- models/review.py
+++ models/review.py
@@ -5,17 +5,24 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 import os
 
+
 class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = 'reviews'
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        place_id = Column(String(60), ForeignKey('places.id', nullable=False))
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
         # Relationships
-        # backref automatically adds a 'reviews' attribute to User and Place models
-        user = relationship("User", backref=backref("reviews", cascade="all, delete-orphan"))
-        place = relationship("Place", backref=backref("reviews", cascade="all, delete-orphan"))
+        # backref automatically adds a 'reviews'
+        # attribute to User and Place models
+        # user = relationship("User", backref=backref("reviews",
+        # cascade="all, delete-orphan"))
+        # place = relationship("Place",
+        # backref=backref("reviews", cascade="all,
+        # delete-orphan"))
+        # user = relationship("User", back_populates="reviews")
+        # place = relationship("Place", back_populates="reviews")
     else:
         place_id = ""
         user_id = ""
