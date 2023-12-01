495b62491508dd7159a62953449bf3b10c53fa98
EsmeraldaMar <6580@holbertonstudents.com>
2023-12-01 15:50:26 +1100

    fix: state.py added empty name string

--- models/state.py
+++ models/state.py
@@ -27,6 +27,7 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-    else:
         cities = relationship("City", cascade="delete, delete-orphan",
                               backref="state")
+    else:
+        name = ""
