df3454da77d68675519a91dfa62a3419f6c883e3
aliciastudies <6722@holbertonstudents.com>
2023-12-11 19:52:29 +1100

    fix: added space to pass pycodestyle

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_route(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text':"is cool"}, strict_slashes=False)
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text):
     return "Python {}".format(text.replace("_", " "))
