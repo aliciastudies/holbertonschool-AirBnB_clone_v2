5ee9e0f7bc1a82b967dae67a20d5fc194afe7431
aliciastudies <6722@holbertonstudents.com>
2023-12-11 18:51:50 +1100

    fix: added module documentation in right spot

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from flask import Flask
 """
 starts a Flask web application
 """
+from flask import Flask
+
 
 app = Flask(__name__)
 # determine root path of the application
@@ -12,6 +13,7 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNH!"
 # define route
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
