9599dbad947b91a5e5a290b13869bbb0ec7787ac
aliciastudies <6722@holbertonstudents.com>
2023-12-11 18:45:49 +1100

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+starts a Flask web application
+"""
+
+app = Flask(__name__)
+# determine root path of the application
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNH!"
+# define route
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
